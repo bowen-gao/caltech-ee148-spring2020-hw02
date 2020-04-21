010665f274b444194c8c6df85b55374d09962152
Bowen Gao <billgao0111@gmail.com>
2020-04-21 04:42:55 -0700

    change dir paths

--- eval_detector.py
+++ eval_detector.py
@@ -2,13 +2,14 @@ import os
 import json
 import numpy as np
 
+
 def compute_iou(box_1, box_2):
     '''
     This function takes a pair of bounding boxes and returns intersection-over-
     union (IoU) of two bounding boxes.
     '''
     iou = np.random.random()
-    
+
     assert (iou >= 0) and (iou <= 1.0)
 
     return iou
@@ -38,21 +39,21 @@ def compute_counts(preds, gts, iou_thr=0.5, conf_thr=0.5):
             for j in range(len(pred)):
                 iou = compute_iou(pred[j][:4], gt[i])
 
-
     '''
     END YOUR CODE
     '''
 
     return TP, FP, FN
 
+
 # set a path for predictions and annotations:
-preds_path = '../data/hw02_preds'
-gts_path = '../data/hw02_annotations'
+preds_path = '/data/hw02_preds'
+gts_path = '/data/hw02_annotations'
 
 # load splits:
-split_path = '../data/hw02_splits'
-file_names_train = np.load(os.path.join(split_path,'file_names_train.npy'))
-file_names_test = np.load(os.path.join(split_Path,'file_names_test.npy'))
+split_path = '/data/hw02_splits'
+file_names_train = np.load(os.path.join(split_path, 'file_names_train.npy'))
+file_names_test = np.load(os.path.join(split_path, 'file_names_test.npy'))
 
 # Set this parameter to True when you're done with algorithm development:
 done_tweaking = False
@@ -60,30 +61,29 @@ done_tweaking = False
 '''
 Load training data. 
 '''
-with open(os.path.join(preds_path,'preds_train.json'),'r') as f:
+with open(os.path.join(preds_path, 'preds_train.json'), 'r') as f:
     preds_train = json.load(f)
-    
-with open(os.path.join(gts_path, 'annotations_train.json'),'r') as f:
+
+with open(os.path.join(gts_path, 'annotations_train.json'), 'r') as f:
     gts_train = json.load(f)
 
 if done_tweaking:
-    
     '''
     Load test data.
     '''
-    
-    with open(os.path.join(preds_path,'preds_test.json'),'r') as f:
-        preds_test = json.load(f)
-        
-    with open(os.path.join(gts_path, 'annotations_test.json'),'r') as f:
-        gts_test = json.load(f)
 
+    with open(os.path.join(preds_path, 'preds_test.json'), 'r') as f:
+        preds_test = json.load(f)
+
+    with open(os.path.join(gts_path, 'annotations_test.json'), 'r') as f:
+        gts_test = json.load(f)
 
 # For a fixed IoU threshold, vary the confidence thresholds.
 # The code below gives an example on the training set for one IoU threshold. 
 
 
-confidence_thrs = np.sort(np.array([preds_train[fname][4] for fname in preds_train],dtype=float)) # using (ascending) list of confidence scores as thresholds
+confidence_thrs = np.sort(np.array([preds_train[fname][4] for fname in preds_train],
+                                   dtype=float))  # using (ascending) list of confidence scores as thresholds
 tp_train = np.zeros(len(confidence_thrs))
 fp_train = np.zeros(len(confidence_thrs))
 fn_train = np.zeros(len(confidence_thrs))
--- generate_split.py
+++ generate_split.py
@@ -1,14 +1,16 @@
 import numpy as np
 import os
+import json
 
-np.random.seed(2020) # to ensure you always get the same train/test split
+np.random.seed(2020)  # to ensure you always get the same train/test split
 
-data_path = '../data/RedLights2011_Medium'
-gts_path = '../data/hw02_annotations'
-split_path = '../data/hw02_splits'
-os.makedirs(preds_path, exist_ok=True) # create directory if needed
+data_path = '/data/RedLights2011_Medium'
+gts_path = '/data/hw02_annotations'
+preds_path = '/data/hw02_preds'
+split_path = '/data/hw02_splits'
+os.makedirs(preds_path, exist_ok=True)  # create directory if needed
 
-split_test = False # set to True and run when annotations are available
+split_test = False  # set to True and run when annotations are available
 
 train_frac = 0.85
 
@@ -26,15 +28,15 @@ Your code below.
 '''
 
 assert (len(file_names_train) + len(file_names_test)) == len(file_names)
-assert len(np.intersect1d(file_names_train,file_names_test)) == 0
+assert len(np.intersect1d(file_names_train, file_names_test)) == 0
 
-np.save(os.path.join(split_path,'file_names_train.npy'),file_names_train)
-np.save(os.path.join(split_path,'file_names_test.npy'),file_names_test)
+np.save(os.path.join(split_path, 'file_names_train.npy'), file_names_train)
+np.save(os.path.join(split_path, 'file_names_test.npy'), file_names_test)
 
 if split_test:
-    with open(os.path.join(gts_path, 'annotations.json'),'r') as f:
+    with open(os.path.join(gts_path, 'annotations.json'), 'r') as f:
         gts = json.load(f)
-    
+
     # Use file_names_train and file_names_test to apply the split to the
     # annotations
     gts_train = {}
@@ -42,11 +44,9 @@ if split_test:
     '''
     Your code below. 
     '''
-    
-    with open(os.path.join(gts_path, 'annotations_train.json'),'w') as f:
-        json.dump(gts_train,f)
-    
-    with open(os.path.join(gts_path, 'annotations_test.json'),'w') as f:
-        json.dump(gts_test,f)
-    
-    
+
+    with open(os.path.join(gts_path, 'annotations_train.json'), 'w') as f:
+        json.dump(gts_train, f)
+
+    with open(os.path.join(gts_path, 'annotations_test.json'), 'w') as f:
+        json.dump(gts_test, f)
