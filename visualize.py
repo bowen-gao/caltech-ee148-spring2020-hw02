3e20a646b4d805797e7480f8dbcf80a1a76f9759
Bowen Gao <billgao0111@gmail.com>
2020-04-23 18:56:01 -0700

    upload visualzie

--- visualize.py
+++ visualize.py
@@ -10,13 +10,15 @@ for filename in preds_train:
     I = Image.open("data/RedLights2011_Medium/" + filename)
     draw = ImageDraw.Draw(I)
     for box in preds_train[filename]:
-        draw.rectangle([box[1], box[0], box[3], box[2]])
+        if box[4] > 0.9:
+            draw.rectangle([box[1], box[0], box[3], box[2]])
     I.save("data/hw02_visualizations/" + filename)
 for filename in preds_test:
     I = Image.open("data/RedLights2011_Medium/" + filename)
     draw = ImageDraw.Draw(I)
     for box in preds_test[filename]:
-        draw.rectangle([box[1], box[0], box[3], box[2]])
+        if box[4] > 0.9:
+            draw.rectangle([box[1], box[0], box[3], box[2]])
     I.save("data/hw02_visualizations/" + filename)
 
 with open('data/hw02_annotations/annotations.json') as f:
@@ -28,5 +30,3 @@ for filename in gts:
         box = [int(i) for i in box]
         draw.rectangle([box[1], box[0], box[3], box[2]])
     I.save("data/hw02_gt/" + filename)
-
-
